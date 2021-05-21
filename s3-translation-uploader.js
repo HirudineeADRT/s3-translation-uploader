e11e3945ad9b75320b802a97c00d7c3b653fb40b
HirudineeADRT <hirudinee@adroitlogic.com>
2021-05-21 12:40:58 +0530

    Removed the Function

--- s3-translation-uploader.js
+++ s3-translation-uploader.js
@@ -7,42 +7,41 @@ exports.handler = async (event) => {
     let sourceData = event.data;
     let sourceLanguage = event.language;
     let fileName = event.filename;
-    let s3Content = await translateInputData();
     let bucketName = "induuuu" // ** Please rename the bucket name to one of your own buckets **
 
-    async function translateInputData() {
-        try {
-            let data = await translate.translateText({
-                SourceLanguageCode: sourceLanguage,
-                TargetLanguageCode: "en",
-                Text: sourceData
-            }).promise();
+    try {
+        let data = await translate.translateText({
+            SourceLanguageCode: sourceLanguage,
+            TargetLanguageCode: "en",
+            Text: sourceData
+        }).promise();
 
-            let translateText = data.TranslatedText;
-            console.log("This is the translation : "+ translateText);
-            return (translateText);
+        let s3Content = data.TranslatedText;
+        console.log("This is the translation : " + s3Content);
+        //return (s3Content);
+        try {
+            let data = await s3.putObject({
+                Bucket: bucketName,
+                Key: fileName,
+                Body: s3Content,
+                Metadata: {}
+            }).promise();
+            console.log(`The object ${fileName} is successfully saved to the S3 bucket ${bucketName}`);
+            return { "message": "Translated Text : " + s3Content };
 
         } catch (err) {
-            msg = `Unable to translate input data due to an error: ${err}`;
-            console.log(msg);
-            throw(err);
-        };
-    }
-
-    try {
-        let data = await s3.putObject({
-            Bucket: bucketName,
-            Key: fileName,
-            Body: s3Content,
-            Metadata: {}
-        }).promise();
-        console.log(`The object ${fileName} is successfully saved to the S3 bucket ${bucketName}`);
-        return { "message": "Translated Text : "+ s3Content };
-
-    } catch (err) {
             msg = `Unable to save the object in to the S3 bucket: ${err}`;
             console.log(msg);
-            throw(err);
+            throw (err);
+        };
+
+    } catch (err) {
+        msg = `Unable to translate input data due to an error: ${err}`;
+        console.log(msg);
+        throw (err);
     };
+
+
+
 
 };
